4144e38cacda803fffd8b7adf135373ad1f0f957
Dylan Jay <git@dylanjay.com>
2008-09-26 17:53:49 +0900

    fixed makeattachments

--- pretaweb/blueprints/makeattachments.py
+++ pretaweb/blueprints/makeattachments.py
@@ -8,29 +8,6 @@ import logging
 logger = logging.getLogger('Plone')
 
 
-"""
-Make Attachments
-================
-
-
-Will look at the backlinks for all Images (or other types) and will
-make those items subitems of what its linked from if that is the only
-place they are linked from.
-By default this will turn a Document into a Folder with a defaultview
-and place the attachments inside that folder. If you don't want that 
-behaviour you can specify field names to use for attachments.
-
-for example
-
-... [attachments]
-... blueprint = pretaweb.blueprints.makeattachments
-... fields = python:{'attachment'+num+'Title': item['title'], 'attachment'+num+'Image': item['image']}
-... condition = python: item.get('_type') in ['Image']
-
-will add attachment1, attachment2 etc fields to the html item.
-
-
-"""
 
 class MakeAttachments(object):
     classProvides(ISectionBlueprint)
@@ -39,7 +16,7 @@ class MakeAttachments(object):
     def __init__(self, transmogrifier, name, options, previous):
         self.previous = previous
         self.fields=Expression(options.get('fields',''), transmogrifier, name, options)
-        self.condition=Condition(options.get('condition',''), transmogrifier, name, options)
+        self.condition=Condition(options.get('condition','python:True'), transmogrifier, name, options)
 
     def __iter__(self):
 
@@ -55,8 +32,6 @@ class MakeAttachments(object):
                 subitems[link].append(item)
             items.append(item)
  
-        #TODO: take care of case where our parent is a subitem
-
         # apply new fields from subitems to items 
         for item in items:
             base = item.get('_site_url',None)
@@ -68,38 +43,51 @@ class MakeAttachments(object):
             #    import pdb; pdb.set_trace()
             #if '_path' in item and item.get('_path','').count('026D.doc'):
             #    import pdb; pdb.set_trace()
-            for i, subitem in enumerate(subitems.get(base+path,[])):
+            if not subitems.get(base+path,[]):
+                continue #item is a deadend and will be delt with elsewhere
+            folder=None
+            i = 0
+            for subitem in subitems.get(base+path,[]):
                 subbase = subitem.get('_site_url',None)
-                subpath = subitem.get('_origin',item.get('_path',None))
+                subpath = subitem.get('_origin',subitem.get('_path',None))
+                    
                 if subitems.get(subbase+subpath,[]):
-                    # we won't make recursive attachments
+                    # subitem isn;t a deadend and will be dealt with elsewhere. 
                     continue
-                if not self.condition(item,i=i+1,subitem=subitem):
-                    yield subitem
+                if not self.condition(item,i=i,subitem=subitem):
+                    yield item
                     continue
-                change = self.fields(item, subitem=subitem, num=i+1)
-                if not change:
-                    yield subitem
-                item.update(dict(change))
-                msg = "imakeattachments: %s to %s{%s}" %(subpath,path,dict(change).keys())
-                logger.log(logging.DEBUG, msg)
-                # now pass a move request to relinker
-                file,text=change[0]
+                change = self.fields(item, subitem=subitem, i=i)
                 #import pdb; pdb.set_trace()
-                newpath = '/'.join(item['_path'].split('/')+[file])
-                yield dict(_origin=subpath,
-                           _path=newpath,
-                           _site_url=subbase)
-            if base+path in subitems:
-                del subitems[base+path]
+                if change:
+                    item.update(dict(change))
+                    msg = "imakeattachments: %s to %s{%s}" %(subpath,path,dict(change).keys())
+                    logger.log(logging.DEBUG, msg)
+                    # now pass a move request to relinker
+                    file,text=change[0]
+                    #import pdb; pdb.set_trace()
+                    newpath = '/'.join(item['_path'].split('/')+[file])
+                    yield dict(_origin=subpath,
+                               _path=newpath,
+                               _site_url=subbase)
+                else: #turn into default folder
+                    if not folder:
+                        folder = dict(_path=item['_path'],
+                                      _site_url=base,
+                                      _type="Folder",
+                                      _defaultpage='index_html')
+                        if not item.get('_origin'):
+                            item['_origin']=path
+                        item['_path'] = '/'.join(item['_path'].split('/') + ['index_html'])
+                    if '_origin' not in subitem:
+                        subitem['_origin'] = subitem['_path']
+                    file = subitem['_path'].split('/')[-1]
+                    subitem['_path'] = '/'.join(folder['_path'].split('/') + [file])
+                    yield subitem
+                i = i +1
+            if folder:
+                yield folder
             yield item
-        #remainder
-        for url,items in subitems.items():
-#            import pdb; pdb.set_trace()
-            
-            for item in items:
-                yield item
 
-
-
+        
 
