cfed343fcb54840b8c25545e1fcb033e20766b5c
Dylan Jay <software@pretaweb.com>
2009-07-13 00:45:02 +1000

    initial UI and tests

---
+++ pretaweb/funnelweb/interfaces.py
@@ -0,0 +1,15 @@
+
+from zope.interface import Interface
+
+
+
+class ISectionFeedback(Interface):
+    """  """
+    def success(section, msg):
+        ""
+    def ignored(section, msg):
+        ""
+
+
+
+
