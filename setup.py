fe6da2e97e81ea0c7d8a7a0503a55fc7425269e2
Dylan Jay <software@pretaweb.com>
2010-01-26 00:18:36 +1100

    hook in test

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.2'
 setup(name='transmogrify.webcrawler',
       version=version,
       description="",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open(os.path.join("transmogrify", "webcrawler", "webcrawler.txt")).read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
