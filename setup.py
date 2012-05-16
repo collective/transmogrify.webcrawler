f403098982e60ba41946ea368e66b1fa3c6d5b24
Dylan Jay <software@pretaweb.com>
2012-05-16 21:08:05 +1000

    clean up readme

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(name='transmogrify.webcrawler',
       version=version,
       description="Crawling and feeding html content into a transmogrifier pipeline",
       long_description=open('README.txt').read() + '\n' +
-                      open(os.path.join("transmogrify", "webcrawler", "webcrawler.txt")).read() + "\n" +
-                        open(os.path.join("transmogrify", "webcrawler", "typerecognitor.txt")).read() + "\n" +
+#                      open(os.path.join("transmogrify", "webcrawler", "webcrawler.txt")).read() + "\n" +
+#                        open(os.path.join("transmogrify", "webcrawler", "typerecognitor.txt")).read() + "\n" +
                         open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
