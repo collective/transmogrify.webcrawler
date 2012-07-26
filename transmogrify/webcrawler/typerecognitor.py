e4473a1c5727d46c8e3b6a4a411d8c19e061ffd5
Dylan Jay <software@pretaweb.com>
2012-07-26 17:47:33 +1000

    turn redirects into Link objects

--- transmogrify/webcrawler/typerecognitor.py
+++ transmogrify/webcrawler/typerecognitor.py
@@ -49,6 +49,11 @@ class TypeRecognitor(object):
             if '_bad_url' in item:
                 yield item; continue
 
+            if not '_type' in item and '_redir' in item:
+                # it's a redirection
+                item['_type'] = 'Link'
+                item['remoteUrl'] = relative_url(item['_path'], item['_redir'])
+
             # needed parameters to be able to recognize
             if '_path' not in item or \
                '_site_url' not in item or \
@@ -102,3 +107,52 @@ class TypeRecognitor(object):
         return dict(_type      = 'File',
                     _mimetype  = ctype)
 
+
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+#
+# Create a relative URL given to absolute URLs
+# See tests at the end:
+# test_relative_url("http://foo/a/b", "http://foo/c", "../c")
+#
+# (c) May 2002 Thomas Guettler http://www.thomas-guettler.de
+# This code is in the public domain
+# Feedback Welcome
+#
+
+import urlparse
+import re
+import string
+
+
+def relative_url(source, target):
+    su=urlparse.urlparse(source)
+    tu=urlparse.urlparse(target)
+    junk=tu[3:]
+    if su[0]!=tu[0] or su[1]!=tu[1]:
+        #scheme (http) or netloc (www.heise.de) are different
+        #return absolut path of target
+        return target
+    su=re.split("/", su[2])
+    tu=re.split("/", tu[2])
+    su.reverse()
+    tu.reverse()
+
+    #remove parts which are equal   (['a', 'b'] ['a', 'c'] --> ['c'])
+    while len(su)>0 and len(tu)>0 and su[-1]==tu[-1]:
+        su.pop()
+        last_pop=tu.pop()
+    if len(su)==0 and len(tu)==0:
+        #Special case: link to itself (http://foo/a http://foo/a -> a)
+        tu.append(last_pop)
+    if len(su)==1 and su[0]=="" and len(tu)==0:
+        #Special case: (http://foo/a/ http://foo/a -> ../a)
+        su.append(last_pop)
+        tu.append(last_pop)
+    tu.reverse()
+    relative_url=[]
+    for i in range(len(su)-1):
+        relative_url.append("..")
+    rel_url=string.join(relative_url + tu, "/")
+    rel_url=urlparse.urlunparse(["", "", rel_url, junk[0], junk[1], junk[2]])
+    return rel_url
